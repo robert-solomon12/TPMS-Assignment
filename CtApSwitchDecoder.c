8f481c4a61bb3b6f4bd8fed7b2fdb4cd6bea5aa1
Robert Solomon <20079462@mail.wit.ie>
2021-01-21 22:56:59 +0000

    Add files via upload

---
+++ CtApSwitchDecoder.c
@@ -0,0 +1,167 @@
+/**********************************************************************************************************************
+ *  FILE REQUIRES USER MODIFICATIONS
+ *  Template Scope: sections marked with Start and End comments
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  This file includes template code that must be completed and/or adapted during BSW integration.
+ *  The template code is incomplete and only intended for providing a signature and an empty implementation.
+ *  It is neither intended nor qualified for use in series production without applying suitable quality measures.
+ *  The template code must be completed as described in the instructions given within this file and/or in the.
+ *  Technical Reference.
+ *  The completed implementation must be tested with diligent care and must comply with all quality requirements which.
+ *  are necessary according to the state of the art before its use.
+ *********************************************************************************************************************/
+/**********************************************************************************************************************
+ *  Implementation template
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  \verbatim
+ *
+ *                This implementation is incomplete and is neither intended nor qualified for use in series production
+ *                without applying suitable quality measures. The implementation as well as any of its modifications
+ *                must be tested with diligent care and must comply with all quality requirements which are necessary
+ *                according to the state of the art before its use.
+ *  \endverbatim
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *             File:  CtApSwitchDecoder.c
+ *        SW-C Type:  CtApSwitchDecoder
+ *  Generation Time:  2020-12-05 01:33:53
+ *
+ *        Generator:  MICROSAR RTE ContractPhase Generator Version 3.14.17
+ *                    RTE Core Version 1.23.0
+ *          License:  DVSwcGen
+ *
+ *      Description:  C-Code implementation template for SW-C <CtApSwitchDecoder>
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of version logging area >>                DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+/* PRQA S 0777, 0779 EOF */ /* MD_MSR_Rule5.1, MD_MSR_Rule5.2 */
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of version logging area >>                  DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+#include "Rte_CtApSwitchDecoder.h"
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of include and declaration area >>        DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of include and declaration area >>          DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ *
+ * Used AUTOSAR Data Types
+ *
+ **********************************************************************************************************************
+ *
+ * Primitive Types:
+ * ================
+ * uint8: Integer in interval [0...255] (standard type)
+ *
+ *********************************************************************************************************************/
+
+
+#define CtApSwitchDecoder_START_SEC_CODE
+#include "CtApSwitchDecoder_MemMap.h" /* PRQA S 5087 */ /* MD_MSR_MemMap */
+
+/**********************************************************************************************************************
+ *
+ * Runnable Entity Name: RCtApSwitchDecoderRunnable
+ *
+ *---------------------------------------------------------------------------------------------------------------------
+ *
+ * Executed if at least one of the following trigger conditions occurred:
+ *   - triggered on TimingEvent every 40ms
+ *
+ **********************************************************************************************************************
+ *
+ * Input Interfaces:
+ * =================
+ *   Explicit S/R API:
+ *   -----------------
+ *   Std_ReturnType Rte_Read_PpFLSenseState_DePiSenseState(Rte_Instance self, uint8 *data)
+ *   Std_ReturnType Rte_Read_PpFRSenseState_DePiSenseState(Rte_Instance self, uint8 *data)
+ *   Std_ReturnType Rte_Read_PpRLSenseState_DePiSenseState(Rte_Instance self, uint8 *data)
+ *   Std_ReturnType Rte_Read_PpRRSenseState_DePiSenseState(Rte_Instance self, uint8 *data)
+ *
+ * Output Interfaces:
+ * ==================
+ *   Explicit S/R API:
+ *   -----------------
+ *   Std_ReturnType Rte_Write_PpLogSig_DePiLedState(Rte_Instance self, uint8 data)
+ *
+ *********************************************************************************************************************/
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of documentation area >>                  DO NOT CHANGE THIS COMMENT!
+ * Symbol: RCtApSwitchDecoderRunnable_doc
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of documentation area >>                    DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+FUNC(void, CtApSwitchDecoder_CODE) RCtApSwitchDecoderRunnable(Rte_Instance self) /* PRQA S 0624, 3206 */ /* MD_Rte_0624, MD_Rte_3206 */
+{
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of runnable implementation >>             DO NOT CHANGE THIS COMMENT!
+ * Symbol: RCtApSwitchDecoderRunnable
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of runnable implementation >>               DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+}
+
+
+#define CtApSwitchDecoder_STOP_SEC_CODE
+#include "CtApSwitchDecoder_MemMap.h" /* PRQA S 5087 */ /* MD_MSR_MemMap */
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of function definition area >>            DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of function definition area >>              DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << Start of removed code area >>                   DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * DO NOT CHANGE THIS COMMENT!           << End of removed code area >>                     DO NOT CHANGE THIS COMMENT!
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ MISRA 2012 violations and justifications
+ *********************************************************************************************************************/
+
+/* module specific MISRA deviations:
+   MD_Rte_0624:  MISRA rule: Rule8.3
+     Reason:     This MISRA violation is a consequence from the RTE requirements [SWS_Rte_01007] [SWS_Rte_01150].
+                 The typedefs are never used in the same context.
+     Risk:       No functional risk. Only a cast to uint8* is performed.
+     Prevention: Not required.
+
+   MD_Rte_3206:  MISRA rule: Rule2.7
+     Reason:     The parameter are not used by the code in all possible code variants.
+     Risk:       No functional risk.
+     Prevention: Not required.
+
+*/
